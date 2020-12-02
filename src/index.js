ee2b0d56f9d3db275105675a6e34c6b5329a725a
Paul Bodelot <49484206+paulbodelot@users.noreply.github.com>
2020-12-02 10:42:08 +0100

    first http request

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-$("#click-submit").click( function (event) {
-    event.preventDefault();
-    let TextQuery = $("#researchField").val();
-    console.log (TextQuery);
-    let sparqlRequest = createSparqlRequest(TextQuery);
-    createHTTPRequest(sparqlRequest);
-    } 
-);
+$(document).ready(function(){
+
+    $("#click-submit").click( function (event) {
+        event.preventDefault();
+        let TextQuery = $("#researchField").val();
+        console.log (TextQuery);
+        let sparqlRequest = createSparqlRequest(TextQuery);
+        createHTTPRequest(sparqlRequest);
+        } 
+    );
+
+});
