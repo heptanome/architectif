f7cec9bf7a6b7d3c08aa9739c3b8789d3457c212
Paul Bodelot <49484206+paulbodelot@users.noreply.github.com>
2020-12-02 11:32:10 +0100

    fix bug request

--- src/js/sparql.js
+++ src/js/sparql.js
@@ -8,16 +8,18 @@ function createSparqlRequest(userRequest) {
     const keyWords = userRequest.split(' ');
     var sparqlRequest = "SELECT DISTINCT ?result WHERE { ?result a dbo:ArchitecturalStructure; rdfs:label ?label. FILTER ( regex(?label, \".*";
     keyWords.forEach(function(item) {
-        sparqlRequest.concat(item);
+        sparqlRequest = sparqlRequest.concat(item);
+        
     });
-    sparqlRequest.concat(,"\"))} LIMIT 200 ");
+    sparqlRequest = sparqlRequest.concat("\"))} LIMIT 200 ");
 
     return sparqlRequest;
 }
 
 function createHTTPRequest(sparqlRequest){
-    let sparqlRequestTest = "SELECT ?s WHERE { ?s rdf:type dbo:ArchitecturalStructure; dbo:location dbr:France. } LIMIT 10";
-    let sparqlRequestTestURI = encodeURI(sparqlRequestTest);
+    //let sparqlRequestTest = "SELECT ?s WHERE { ?s rdf:type dbo:ArchitecturalStructure; dbo:location dbr:France. } LIMIT 10";
+    console.log(sparqlRequest);
+    let sparqlRequestTestURI = encodeURI(sparqlRequest);
     let baseURL = "http://dbpedia.org/sparql?default-graph-uri=http%3A//dbpedia.org&query=PREFIX%20owl%3A%20%3Chttp%3A//www.w3.org/2002/07/owl%23%3E%0APREFIX%20xsd%3A%20%3Chttp%3A//www.w3.org/2001/XMLSchema%23%3E%0APREFIX%20rdfs%3A%20%3Chttp%3A//www.w3.org/2000/01/rdf-schema%23%3E%0APREFIX%20rdf%3A%20%3Chttp%3A//www.w3.org/1999/02/22-rdf-syntax-ns%23%3E%0APREFIX%20foaf%3A%20%3Chttp%3A//xmlns.com/foaf/0.1/%3E%0APREFIX%20dc%3A%20%3Chttp%3A//purl.org/dc/elements/1.1/%3E%0APREFIX%20%3A%20%3Chttp%3A//dbpedia.org/resource/%3E%0APREFIX%20dbpedia2%3A%20%3Chttp%3A//dbpedia.org/property/%3E%0APREFIX%20dbpedia%3A%20%3Chttp%3A//dbpedia.org/%3E%0APREFIX%20skos%3A%20%3Chttp://www.w3.org/2004/02/skos/core%23%3E%0A";
     let endURL = "&format=application/sparql-results%2Bjson";
     let baseURLFull = baseURL.concat(sparqlRequestTestURI, endURL);
