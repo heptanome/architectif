a2dcbb69ffe4caebe9fac1c0317a8078b327d93a
INSArthur <49277717+INSArthur@users.noreply.github.com>
2020-12-02 10:55:11 +0100

    complete createSparqlRequest

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
     $("#click-submit").click( function (event) {
         event.preventDefault();
-        let TextQuery = $("#researchField").val();
-        console.log (TextQuery);
-        let sparqlRequest = createSparqlRequest(TextQuery);
+        let userRequest = $("#researchField").val();
+        console.log (userRequest);
+        let sparqlRequest = createSparqlRequest(userRequest);
         createHTTPRequest(sparqlRequest);
         } 
     );
