c3330e1dde69797b246beeb138c1d3cc3d693d71
Guillaume <guillaume@kessibi.fr>
2020-12-07 22:57:10 +0100

    index: now correctly sends to results page

--- src/js/index.js
+++ src/js/index.js
@@ -3,90 +3,97 @@ Gestion de l'évènement "click sur le bouton submit principale".
 Sous-traite la création de requête sparql et Http à Sparql.js
 Envoie la requête, récupère son résultat et l'affiche
 */
-$("#click-submit").click( function (event) {
-    event.preventDefault();
-    let userRequest = $("#researchField").val();
-    console.log (userRequest);
-    let sparqlRequest = createSparqlRequest(userRequest);
-    let baseURLFull = createHTTPRequest(sparqlRequest);
+$("#click-submit").click(function (event) {
+  event.preventDefault();
+  let userRequest = $("#researchField").val();
+  console.log(userRequest);
+  let sparqlRequest = createSparqlRequest(userRequest);
+  let baseURLFull = createHTTPRequest(sparqlRequest);
 
-    //Envoie, récupération et affichage de la requête HTTP
-    $.get( baseURLFull, function( data ) {
-        displayListResult(data);
-    });
-
-    }
-);
+  //Envoie, récupération et affichage de la requête HTTP
+  $.get(baseURLFull, function (data) {
+    displayListResult(data);
+  });
+});
 
 $("#click-display").click(function () {
-    $(".table tbody tr").show();
-})
+  $(".table tbody tr").show();
+});
 
 /*
 Afficher la liste de résultat de la recherche
 */
-function displayListResult(data){
-    console.log(data);
-    var sizeOfResults = data.results.bindings.length;
-    var sizeOfResultsDisplayed = $(".custom-select option:selected").html();
-    var resultsDisplayed = 0;
-    var resultsLoaded = 1;
-    var liste2 = "";
-    if (sizeOfResults > 0) {
-        var liste = "";
-        var results = data.results.bindings
-        $(results).each(function (){
-            var place = "";
-            if ("place" in this) {
-                place = this.place.value;
-            } else {
-                place = "N/A";
-            }
-            if (resultsDisplayed < sizeOfResultsDisplayed) {
-                res = createLigne(resultsLoaded, this.name.value, place, this.result.value, "");
-                liste2 += res;
-                resultsDisplayed++;
-                resultsLoaded++;
-            } else {
-                res = createLigne(resultsLoaded, this.name.value, place, this.result.value, "display:none;");
-                liste2 += res;
-                resultsLoaded++;
-            }
-        });
-        if (resultsDisplayed < sizeOfResults) {
-            $("#click-display").show()
-            .html("Display all results ("+sizeOfResults+")");
-        } else {
-            $("#click-display").hide()
-            .html("Display all results");
-        }
-        
-        $(".table tbody").html(liste2);
-        $("#result_list").html("");
-    
-        /*Gestion de l'évènement "click sur un lien de la liste de résultats*/
-        $(".table tbody tr").click(function(){
-            var link = $(this).attr("link");
-            document.location.href = "result.html?data=" + link;
-        });
+function displayListResult(data) {
+  console.log(data);
+  var sizeOfResults = data.results.bindings.length;
+  var sizeOfResultsDisplayed = $(".custom-select option:selected").html();
+  var resultsDisplayed = 0;
+  var resultsLoaded = 1;
+  var liste2 = "";
+  if (sizeOfResults > 0) {
+    var liste = "";
+    var results = data.results.bindings;
+    $(results).each(function () {
+      var place = "";
+      if ("place" in this) {
+        place = this.place.value;
+      } else {
+        place = "N/A";
+      }
+      if (resultsDisplayed < sizeOfResultsDisplayed) {
+        res = createLigne(
+          resultsLoaded,
+          this.name.value,
+          place,
+          this.result.value,
+          ""
+        );
+        liste2 += res;
+        resultsDisplayed++;
+        resultsLoaded++;
+      } else {
+        res = createLigne(
+          resultsLoaded,
+          this.name.value,
+          place,
+          this.result.value,
+          "display:none;"
+        );
+        liste2 += res;
+        resultsLoaded++;
+      }
+    });
+    if (resultsDisplayed < sizeOfResults) {
+      $("#click-display")
+        .show()
+        .html("Display all results (" + sizeOfResults + ")");
     } else {
-        $("#result_list").html("Pas de résultats !");
-        $(".table tbody").html(liste2);
+      $("#click-display").hide().html("Display all results");
     }
+
+    $(".table tbody").html(liste2);
+    $("#result_list").html("");
+
+    /*Gestion de l'évènement "click sur un lien de la liste de résultats*/
+    $(".table tbody tr").click(function () {
+      var link = $(this).attr("link");
+      link = link.split("/");
+
+      document.location.href = "result.html?b=" + link[link.length - 1];
+    });
+  } else {
+    $("#result_list").html("Pas de résultats !");
+    $(".table tbody").html(liste2);
+  }
 }
 
-function createLigne (numero, name, place, link, cssStyle){
-    var htmlLigne = 
-     `
+function createLigne(numero, name, place, link, cssStyle) {
+  var htmlLigne = `
     <tr style="${cssStyle}" link="${link}">
       <th scope="row">${numero}</th>
       <td>${name}</td>
       <td>${place}</td>
     </tr>
-    ` ;
-    return htmlLigne;
-};
-
-
-
-
+    `;
+  return htmlLigne;
+}
